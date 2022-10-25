e92eb519bdd73ced41fed9d005f89fe8cda05691
aadhyaputtur <putturaadhya@gmail.com>
2022-10-25 13:18:41 -0400

    dictionary

--- Wrapper.py
+++ Wrapper.py
@@ -5,17 +5,23 @@ import cv2 as cv2
 
 #read matching 1 txt file
 
+
+
 with open('./P3Data/matching1.txt') as f:
+
     lines = f.readlines()
-    for i in range(len(lines)):
+    lengthLines = 3
+    matching_1 = {}
+    for i in range(lengthLines):
         if( i != 0):
+            points_data = {}
             data = lines[i]
             data = data.split()
     
 
       
             print(data)
-            points = data[0]
+            lenpoints = int(data[0])
             r = data[1]
             g = data[2]
             b = data[3]
@@ -23,9 +29,35 @@ with open('./P3Data/matching1.txt') as f:
             imgx = data[4]
             imgy = data[5]
 
-            rgb = [r, g, b]
+            rgb = tuple([r, g, b, imgx, imgy])
+
+
 
             print("RGB ", rgb)
-            #print("points ", points)
+            #data of other points start after data[5]
+
+            total = j = 6
+            while j  < total + lenpoints  + 1:
+                ID = data[j]
+                img_u = data[j + 1]
+                img_v = data[j + 2]
+                j = j + 3
+                print("j ", j)
+                print(" total ", total + lenpoints)
+                points_data[ID] = [img_u, img_v]
+                print("points data", points_data)
+
+
+            matching_1[rgb] = points_data
+
+    print(matching_1)
+
+#Now choose 8 correspondances
+#img1 needs 8 and its corresponding image, (lets say 2)
+for keys in matching_1:
+    
+
+
+
 
 
