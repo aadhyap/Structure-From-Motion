3ad332dbbd4885d5b8b95f7c24ec297fe16f0140
aadhyaputtur <putturaadhya@gmail.com>
2022-10-31 17:58:17 -0400

    I

--- LinearTriangulation.py
+++ LinearTriangulation.py
@@ -12,10 +12,25 @@ class LinearTriangulation:
 
         #Testing each of the matchings and getting an X world points out of them 
 
+        I = array([[1,0,0],[0,1,0],[0,0,1]])
+    
+
         for keys in matchings:
             if imgID in matchings[keys]:
                 currentimg = tuple([keys[3], keys[4]])
                 imgpoints[currentimg] = matchings[keys][imgID]
+                #img points for x and x'
+
+
+        for keys in imgpoints:
+            x = keys[0]
+            y = keys[1]
+
+            x_2, y_2 = imgpoints[keys][0], imgpoints[keys][1]
+
+
+
+
 
         
             
