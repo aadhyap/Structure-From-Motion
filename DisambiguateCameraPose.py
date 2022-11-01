5404d568f49df98edbfa773335bc5129c4fa17b3
aadhyaputtur <putturaadhya@gmail.com>
2022-11-01 13:19:33 -0400

    Disambiguate points

---
+++ DisambiguateCameraPose.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy import linalg
+import math
+from scipy.stats import skew
+
+
+
+class DisambiguateCameraPose:
+
+    def __init__(self, allworldpts):
+
+
+        self.maxpts = 0
+        self.maxCamerapose = None
+        for camerapose in allworldpts:
+
+            C = camerapose[0].reshape(-1,1) 
+            R = camerapose[1].reshape(-1,1) 
+            R = R[2, :].reshape(1,-1)
+
+            worldpts = allworldpts[camerapose]
+
+            numpts = self.numberOfPoints(worldpts)
+
+            if(numpts > self.maxpts):
+                self.maxpts = numpts
+                self.maxCamerapose = camerapose
+
+
+
+
+    def numberOfPoints(self, C, R, worldpts):
+
+        numberpts = 0
+
+        for worldpt in worldpts:
+            if R.dot(worldpt-C)>0 and worldpt[2]>0:
+                numberpts = numberpts + 1:
+
+        return numberpts
+
+
+
