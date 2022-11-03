0aa3481bd0f8b127a59f1aa57e16596fbe2ef0f8
aadhyaputtur <putturaadhya@gmail.com>
2022-11-03 09:29:18 -0400

    geometric error for nonlinear

--- NonlinearTriangulation.py
+++ NonlinearTriangulation.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import linalg
 import math
 from scipy.stats import skew
+import scipy.optimize as optimize
 
 
 class NonlinearTriangulation:
@@ -25,8 +26,13 @@ class NonlinearTriangulation:
         P2 = np.dot(K, np.dot(R2, np.hstack((I, -C2))))
 
 
+        optimized_X = []
         #is it only the world points that made it in this projection
         for i in range(len(allworldpts)):
+            geometric_error = optimize.least_squares(fun=GeometricError, x0=allworldpts[i], method="trf", args=[P1, P2, x1[i], x2[i]])
+            optimized_X.append(optimized_params.x)
+            # x3D_.append(X1[:3])
+        return np.array(optimized_X)
 
 
 
@@ -35,7 +41,27 @@ class NonlinearTriangulation:
 
 
 
-    def GeometricError(x1, x2, X, P1, P2):
+
+
+    def GeometricError(X, P1, P2, x1, x2):
+
+        P1_1, P1_2, P1_3 = P1
+
+        geo_err1 = np.square(np.divide( np.dot(P1_1, X), np.dot(P1_3, X) ) - x) + np.square(np.divide( np.dot(P1_2, X), np.dot(P1_3, X) ) - y) 
+
+
+        P2_1, P2_2, P2_3 = P2
+
+        geo_err2 = np.square(np.divide(np.dot(P2_1, X), np.dot(P2_3, X) ) - x) + np.square(np.divide( np.dot(P2_2, X),np.dot(P2_3, X) ) - y) 
+
+        error = geo_err1 + geo_err2
+
+        return error.squeeze()
+
+
+
+
+
 
         
 
