85150f6ef61d06a6fb894f373b8d617b0780723f
aadhyaputtur <putturaadhya@gmail.com>
2022-11-07 11:05:45 -0500

    edited geometric error for appropriate equation

--- PnPRANSAC.py
+++ PnPRANSAC.py
@@ -11,50 +11,68 @@ class PnPRANSAC:
 
         
 
+        allmatchings = {}
+
         size_matchings = 0
         for pts in imgtoX:
             x2 = pts[1][0:2]     
             if(x2 in new_matchings):
                 size_matchings += 1
+                new_img = new_matchings[x2]
+                worldpt = imgtoX[pts]
+                allmatchings[tuple(new_img)] = worldpt
 
         self.size_matchings = size_matchings #number of coresspondance in dictionary
+        self.RANSAC(allmatchings)
 
         
 
-    def RANSAC(self, sixpoints):
+    def RANSAC(self, allimgpts):
 
         n = {}
         bestres = 0
         for i in range(30):
+            #choose 6 points
+            imgpoints = self.choose6(new_matchings, imgtoX)
+            PnP = LinearPnP(imgpoints, K)
+            C, R, P= PnP.getPose()
+            S = {} #inliers
+
+            for keys in allimgpts:
+                x = keys[0]
+                y = keys[1]
+                z = 1
+                image = np.array([x, y, z])
+
+                worldpt = allimgpts[keys]
+
+                error = self.GeometricError(P, image, worldpt)
+
+                if(error  < 0.05):
+                    
 
 
-
-
-
-
-        
-        imgpoints = self.choose6(new_matchings, imgtoX)
-
-
-
-
-
-
-
-
-
-
-
-
-
-        PnP = LinearPnP(imgpoints, K)
-        C, R = PnP.getPose()
 
         print("<=======================Final Camera Pose===========================>")
         print(C, R)
 
 
+    def GeometricError(self, P, x, X):
 
+            print("CURRENT X ", X)
+
+            P1, P2, P3 = P
+
+            u = x[0]
+            v = x[1]
+
+            X = np.append(X, 1)
+
+
+            error = np.square((u - np.divide(np.dot(P1.T, X), np.dot(P3.T, X)))) + np.square((v - np.divide(np.dot(P2.T, X), np.dot(P3.T, X)))) 
+
+
+            return error.squeeze()
 
 
     def choose6(self, new_matchings, imgtoX):
