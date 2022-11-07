a0bee8aacc21b983742864263e660ff42d56fbea
aadhyaputtur <putturaadhya@gmail.com>
2022-11-07 09:16:52 -0500

    finding 6 random img points

--- PnPRANSAC.py
+++ PnPRANSAC.py
@@ -6,4 +6,46 @@ import math
 
 class PnPRANSAC:
 
-    def __init__(self):
+    def __init__(self, new_matchings, imgtoX):
+
+        PnP = LinearPnP(matching_2, imgToX, K)
+
+        size_matchings = 0
+        for pts in worldpointstoImage:
+            x2 = pts[1][0:2]     
+            if(x2 in new_matchings):
+                size_matchings += 1
+
+        self.size_matchings = size_matchings #number of coresspondance in dictionary
+
+
+
+
+    def choose6(self, new_matchings, imgtoX):
+
+
+        newimgpts = {}
+
+        rangenums =  sample(range(0, self.size_matchings), 6) 
+        print("length of list ", len(rangenums))
+        
+
+        for num in rangenums:
+            count = 0
+            for pts in worldpointstoImage:
+                x2 = pts[1][0:2]
+                if(x2 in new_matchings) and (count == num) :
+                    #image2 match with new image pts
+                    new_img = new_matchings[x2]
+                    worldpt = worldpointstoImage[pts]
+                    newimgpts[tuple(new_img)] = worldpt
+                
+                count +=  1
+
+        #returns newimgpts with correct new image points correspondace with world points
+        return newimgpts
+
+
+
+
+
