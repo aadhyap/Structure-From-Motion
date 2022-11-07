c0963cb650a8a546e52537bf3c8b4654c65f47c8
aadhyaputtur <putturaadhya@gmail.com>
2022-11-07 06:37:44 -0500

    Getting new image points and setting up data for calculation

--- LinearPnP.py
+++ LinearPnP.py
@@ -16,6 +16,57 @@ Outputs:
  R - size (3 x 3) pose rotation
 '''
 
-    def __init__(self, X, x, K):
+
+'''
+Parameters
+new_matchings --> The matchings for the new image (for example image 2 points to image 3 points)
+worldpointstoImage --> Image 1 and Image 2 --> Image 3
+
+
+
+
+'''
+    def __init__(self, new_matchings, worldpointstoImage, K):
+
+       newimgpts = self.getNewImg_pts()
+
+       for pt in newimgpts:
+            u, v = pt
+            imgCross = np.array([[0, -1, v],
+                            [1,  0 , -u],
+                            [-v, u, 0]])
+
+
+            4zero = np.zeros((1, 4))
+            X = newimgpts[pt].reshape((1, 4))
+            Tilde_X = np.vstack((np.hstack((X, 4zero, 4zero)), 
+                            np.hstack((4zero,X, 4zero)), 
+                            np.hstack((4zero, 4zero,X))))
+
+
+
+
+
+
+    
+
+
+    def getNewImg_pts(new_matchings, worldpointstoImage):
+
+
+        newimgpts = {}
+        for pts in worldpointstoImage:
+            x2 = pts[1]
+            if(x2 in new_matchings):
+                #image2 match with new image pts
+                new_img = new_matchings[x2]
+                worldpt = worldpointstoImage[pts]
+                newimgpts[new_img] = worldpt
+
+        #returns newimgpts with correct new image points correspondace with world points
+        return newimgpts
+
+
+
 
         
