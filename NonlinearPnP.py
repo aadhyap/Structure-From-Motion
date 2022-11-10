fbc2acbe344c16c48289506e26805a57c55557c8
aadhyaputtur <putturaadhya@gmail.com>
2022-11-10 17:18:55 -0500

    finished nonlinear PnP

--- NonlinearPnP.py
+++ NonlinearPnP.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.spatial.transform import Rotation 
 import math
+import scipy.optimize as optimize
 
 
 class NonlinearPnP:
@@ -10,16 +11,18 @@ class NonlinearPnP:
         Q = q.as_quat()
         Quart = [Q[0] ,Q[1],Q[2],Q[3]]
         C = [C[0], C[1], C[2]] 
+        params = C + Quart
 
 
-        optimizeX = optimize.least_squares(
-        fun = getLoss,
-        x0=Quart,
-        x1 = C,
+        optimize_QC = optimize.least_squares(
+        fun = self.getLoss,
+        x0=params,
         method="trf",
         args=[pts, K])
 
-        Q, C = optimized_params.x
+        params = optimize_QC.x
+        C = params[:3]
+        Q = params[3:]
         R = Rotation.from_quat(Q)
         self.R = R.as_matrix()
         self.C = C
@@ -29,10 +32,14 @@ class NonlinearPnP:
         return self.C,self.R
         
 
-    def getLoss(self, Quart, C2, pts,  K):
+    def getLoss(self, params, pts,  K):
+        C = params[:3]
+        Q = params[3:]
         C = C.reshape(-1,1)
         R = Rotation.from_quat(Q)
         R = R.as_matrix()
+
+
         P = self.getProjection(C, R, K)
 
 
@@ -44,10 +51,10 @@ class NonlinearPnP:
             X = pts[imagepts]
             P1_1, P1_2, P1_3 = P
 
-            x = x1[0]
-            y = x1[1]
+            x = imagepts[0]
+            y = imagepts[1]
 
-            X = np.append(X, 1)
+            #X = np.append(X, 1)
 
             alg_err = np.square(np.divide( np.dot(P1_1.reshape(1,-1), X), np.dot(P1_3.reshape(1,-1), X) ) - x) + np.square(np.divide( np.dot(P1_2.reshape(1,-1), X), np.dot(P1_3.reshape(1,-1), X) ) - y) 
             error = alg_err
@@ -63,16 +70,16 @@ class NonlinearPnP:
 
 
     def getProjection(self, C2, R2, K):
-        C1 = np.zeros((3,1))
-        R1 = np.identity(3)
-
         C2 = np.array(C2)
         C2 = np.reshape(C2, (3, 1))
         R2 = np.array(R2)
         I = np.array([[1,0,0],[0,1,0],[0,0,1]])
  
+        P = np.dot(K, np.dot(R2, np.hstack((I, -C2))))
 
-        P1 = np.dot(K, np.dot(R1, np.hstack((I, -C1))))
-        P2 = np.dot(K, np.dot(R2, np.hstack((I, -C2))))
+        return P
+
+
+
 
         
