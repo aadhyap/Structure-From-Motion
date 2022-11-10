0977b87cdde20eb08764d27e475fcc68f5c4e371
aadhyaputtur <putturaadhya@gmail.com>
2022-11-10 16:33:49 -0500

    finished functionality of nonlinearPnP

--- NonlinearPnP.py
+++ NonlinearPnP.py
@@ -14,22 +14,58 @@ class NonlinearPnP:
 
         optimizeX = optimize.least_squares(
         fun = getLoss,
-        x0=M,
+        x0=Quart,
+        x1 = C,
         method="trf",
         args=[pts, K, Q, C])
+
+        Q, C = optimized_params.x
+        R = Rotation.from_quat(Q)
+        self.R = R.as_matrix()
+        self.C = C
+
+
+    def getPose(self):
+        return self.C,self.R
+        
 
 
 
 
     def getLoss(self, M, pts,  K, Q, C):
         C = C.reshape(-1,1)
-       
         R = Rotation.from_quat(Q)
         R = R.as_matrix()
+        P = self.getProjection(C, R, K)
+
+
+        allerrors = []
+        for imagepts in pts:
+            x = imagepts[0]
+            y = imagepts[1]
+
+            X = pts[imagepts]
+            P1_1, P1_2, P1_3 = P
+
+            x = x1[0]
+            y = x1[1]
+
+            X = np.append(X, 1)
+
+            alg_err = np.square(np.divide( np.dot(P1_1.reshape(1,-1), X), np.dot(P1_3.reshape(1,-1), X) ) - x) + np.square(np.divide( np.dot(P1_2.reshape(1,-1), X), np.dot(P1_3.reshape(1,-1), X) ) - y) 
+            error = alg_err
+            allerrors.append(error)
 
 
 
-    def getProjection(self, C2, R2):
+        allerrors = np.array(allerrors).squeeze()
+        mean_error = np.mean(allerrors)
+
+        return mean_error
+
+
+
+    def getProjection(self, C2, R2, K):
         C1 = np.zeros((3,1))
         R1 = np.identity(3)
 
